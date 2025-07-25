9987a89bf6f8844a000687e7a551158b41c708d6
iolmao <iolao.omfg@gmail.com>
2025-07-25 15:39:12 +0200

    Update and rename bu_vite.config.js to vite.config.js
    vite adjustments

--- src/admin/vite.config.js
+++ src/admin/vite.config.js
@@ -3,7 +3,7 @@ export default (config) => {
   config.server = {
     config.server,
     host: '0.0.0.0',
-    allowedHosts: 'all'
+    allowedHosts: 'https://strapi-production-6792.up.railway.app'
   };
   
   return config;
